0356089068da0df526b3edd68667db093327ecab
taha560 <taha102050@gmail.com>
2016-05-29 08:29:54 -0700

    Update support.lua

--- plugins/support.lua
+++ plugins/support.lua
@@ -6,7 +6,7 @@ local function callback(extra, success, result)
 end
 
 local function run(msg, matches)
-  local user = 24709501
+  local user = 189308877
 
   if matches[1] == "support" then
     user = 'user#id'..user
